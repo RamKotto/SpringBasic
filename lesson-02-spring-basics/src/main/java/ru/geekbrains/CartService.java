1c3ba69dacdeb2cbb7c4d47a8fa7bf1141759566
d.saraev <d.saraev@a1qa.com>
2021-12-27 17:59:50 +0300

    home work

--- lesson-02-spring-basics/src/main/java/ru/geekbrains/CartService.java
+++ lesson-02-spring-basics/src/main/java/ru/geekbrains/CartService.java
@@ -1,10 +1,27 @@
 package ru.geekbrains;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import ru.geekbrains.persist.Product;
 import ru.geekbrains.persist.ProductRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartService {
+    private List<Product> productCart = new ArrayList<>();
 
     @Autowired
     private ProductRepository productRepository;
+
+    public void putProductById(long id) {
+        productCart.add(productRepository.findById(id));
+    }
+
+    public List<Product> getCart() {
+        return productCart;
+    }
+
+    public void removeFromCartById(long id) {
+        productCart.remove(productRepository.findById(id));
+    }
 }
--- lesson-02-spring-basics/src/main/java/ru/geekbrains/Main.java
+++ lesson-02-spring-basics/src/main/java/ru/geekbrains/Main.java
@@ -2,8 +2,7 @@ package ru.geekbrains;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.w3c.dom.ls.LSOutput;
+import ru.geekbrains.persist.Product;
 import ru.geekbrains.persist.ProductRepository;
 import ru.geekbrains.persist.ProductRepositoryImpl;
 
@@ -25,5 +24,33 @@ public class Main {
         System.out.println("Cart name: " + cartServiceTwo);
 
         System.out.println(productService.count());
+
+        cartServiceOne.putProductById(1);
+        cartServiceOne.putProductById(2);
+        cartServiceTwo.putProductById(3);
+        cartServiceTwo.putProductById(4);
+        cartServiceTwo.putProductById(5);
+
+        for (Product product : cartServiceOne.getCart()) {
+            System.out.println("Cart one: " + product.getName());
+        }
+
+        for (Product product : cartServiceTwo.getCart()) {
+            System.out.println("Cart two: " + product.getName());
+        }
+
+        cartServiceOne.removeFromCartById(1);
+        cartServiceOne.removeFromCartById(2);
+        cartServiceTwo.removeFromCartById(3);
+        cartServiceTwo.removeFromCartById(4);
+        cartServiceTwo.removeFromCartById(5);
+
+        if (cartServiceOne.getCart().size() <= 0) {
+            System.out.println("cartServiceOne is empty");
+        }
+
+        if (cartServiceOne.getCart().size() <= 0) {
+            System.out.println("cartServiceTwo is empty");
+        }
     }
 }
