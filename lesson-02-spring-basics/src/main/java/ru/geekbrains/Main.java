aa015af2bb1a4d09386b363ea703460a1e34cfb4
d.saraev <d.saraev@a1qa.com>
2021-12-27 15:42:46 +0300

    config class

--- lesson-02-spring-basics/src/main/java/ru/geekbrains/Main.java
+++ lesson-02-spring-basics/src/main/java/ru/geekbrains/Main.java
@@ -1,13 +1,19 @@
 package ru.geekbrains;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.geekbrains.persist.ProductRepository;
 import ru.geekbrains.persist.ProductRepositoryImpl;
 
 public class Main {
 
     public static void main(String[] args) {
-        ProductRepository productRepository = new ProductRepositoryImpl();
-        ProductService productService = new ProductService(productRepository);
+//        ProductRepository productRepository = new ProductRepositoryImpl();
+//        ProductService productService = new ProductService(productRepository);
+
+        // IOC container
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+        ProductService productService = context.getBean("productService", ProductService.class);
         System.out.println("Product count: " + productService.count());
     }
 }
--- lesson-02-spring-basics/src/main/java/ru/geekbrains/ProductService.java
+++ lesson-02-spring-basics/src/main/java/ru/geekbrains/ProductService.java
@@ -1,11 +1,15 @@
 package ru.geekbrains;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import ru.geekbrains.persist.ProductRepository;
 
+@Component
 public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
